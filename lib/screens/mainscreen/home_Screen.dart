b88e1055ef62ebb239dc6873fd268f1e0f01fa64
DmiPC <amin.dmi1375@gmail.com>
2024-10-02 21:39:01 +0330

    "Custom ListView implementation"

--- lib/screens/mainscreen/home_Screen.dart
+++ lib/screens/mainscreen/home_Screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:watch_store/components/text_style.dart';
+import 'package:flutter/rendering.dart';
 import 'package:watch_store/gen/assets.gen.dart';
 import 'package:watch_store/res/colors.dart';
 import 'package:watch_store/res/dimens.dart';
@@ -7,7 +7,7 @@ import 'package:watch_store/res/strings.dart';
 import 'package:watch_store/widgets/app_slider.dart';
 import 'package:watch_store/widgets/cat_widget.dart';
 import 'package:watch_store/widgets/search_bar.dart';
-
+import 'package:flutter_svg/flutter_svg.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
@@ -49,11 +49,61 @@ class HomeScreen extends StatelessWidget {
                       ontap: () {},
                       title: AppStrings.classic),
                 ],
+              ),
+              const SizedBox(height: AppDimens.large,),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                reverse: true,
+                child: Row(
+                  children: [
+                    SizedBox(
+                      height: 300,
+                      child: ListView.builder(
+                          physics: ClampingScrollPhysics(),
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 8,
+                          shrinkWrap: true,
+                          itemBuilder: (context, index) => Container(
+                                margin: const EdgeInsets.all(AppDimens.medium),
+                                width: 200,
+                                height: 298,
+                                color: Colors.red,
+                              )),
+                    ),
+                   VerticalText(),
+                  ],
+                ),
               )
             ],
           ),
         ),
       ),
     );
+  }
+}
+
+
+class VerticalText extends StatelessWidget {
+  const VerticalText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: RotatedBox(
+        quarterTurns: -1,
+        child: Column(
+          children: [
+            Row(
+              children: [              
+                SvgPicture.asset(Assets.svg.back),
+                Text("مشاهده همه"),
+              ],          
+            ),
+            Text("شگفت انگیز")
+          ],
+        ),
+      ),
+    ) ;
   }
 }
